574fcdd6d74dd5210887f731a95bb940ecb0072b
alisondev <araujoalison304@gmail.com>
2025-02-02 11:30:10 -0300

    'corrigindo_layout'

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -4,25 +4,21 @@ import Header from "../header";
 export default function Home() {
   return (
     <div className="conteiner">
-
       <Header />
 
       <div className="item">
         <div className="desc">
-          <div>
+          <div className="areaDesc">
             <h2 className="subtitle">Olá, meu nome é Alison Araújo</h2>
             <h3 className="textDesc">
-              Eu sou desenvolvedor Mobile/front-end, Apaixonado por tecnologia e{" "}
+              Eu sou desenvolvedor Mobile/front-end, <br />
+              Apaixonado por tecnologia e programação,
+              <br /> atuo como desenvolvedor<br /> mobile e front-end júnior.<br /> Gosto de
+              construir soluções inovadoras<br /> que facilitam a vida das
               <br />
-              programação, atuo como desenvolvedor mobile e front-end júnior.
-              Gosto de construir <br /> soluções inovadoras que facilitam a vida
-              das <br />
-              pessoas. Tenho experiência em desenvolvimento de aplicativos para{" "}
-              Android e Ios,
-              <br /> e criação de sistemas web.
-              <br />
-              Sou Apaixonado por tecnologia e busco constantemente me aprimorar
-              nessa área.
+              pessoas. Tenho experiência em desenvolvimento<br /> de aplicativos para
+              Android e Ios,<br /> e criação de sistemas web. Sou Apaixonado,<br /> por
+              tecnologia e busco constantemente<br /> me aprimorar nessa área.
             </h3>
           </div>
 
@@ -56,7 +52,7 @@ export default function Home() {
             </p>
             <a href="https://carteiraweb.vercel.app/">Acessar</a>
           </div>
-          <div className="idProj" >
+          <div className="idProj">
             <img src="https://github.com/AlisonNunesAraujo/appcompras/blob/main/assets/app.jpg?raw=true" />
             <h3>Aplicativo lista de compras</h3>
             <p>
@@ -73,22 +69,25 @@ export default function Home() {
           <h3>Skills</h3>
         </div>
 
-
         <div className="areaSkills">
           <div className="areaFront">
             <h3>Front-End</h3>
             <img src="https://camo.githubusercontent.com/426c1121b29abc64a6b1af1e3aa3091abb38e39c87054720b765af1425c74e7f/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6a6176617363726970742f6a6176617363726970742d6f726967696e616c2e737667" />{" "}
+            <p>JavaScript</p>
             <img src="https://camo.githubusercontent.com/a07203131922e3fa0d6d0cd787edb5597771b30d712574bbc70a3c7aaa0161ea/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f747970657363726970742f747970657363726970742d6f726967696e616c2e737667" />{" "}
+            <p>TypeScript</p>
             <img src="https://camo.githubusercontent.com/34b891c76d258e4b0ee593443e5cbc2506cdbb7d3cd6bc0e4beffa87a9c1611b/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f72656163742f72656163742d6f726967696e616c2e737667" />{" "}
+            <p>ReactJs</p>
           </div>
 
           <div className="areaMobile">
             <h3>Mobile</h3>
             <img src="https://camo.githubusercontent.com/34b891c76d258e4b0ee593443e5cbc2506cdbb7d3cd6bc0e4beffa87a9c1611b/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f72656163742f72656163742d6f726967696e616c2e737667" />{" "}
+            <p>React Native</p>
             <img src="https://camo.githubusercontent.com/a07203131922e3fa0d6d0cd787edb5597771b30d712574bbc70a3c7aaa0161ea/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f747970657363726970742f747970657363726970742d6f726967696e616c2e737667" />
+            <p>TypeScript</p>
           </div>
         </div>
-
 
         <div className="contato">
           <div className="areaContato">
